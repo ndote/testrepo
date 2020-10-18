bda32462c6d08cdfdb9a594da6dec977b718fea1
ndote <56348299+ndote@users.noreply.github.com>
2020-10-18 18:04:36 +1100

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,1 @@
+#first python in github wala koy kaingon for now nagajuggle ko between photohgraphy data, reading books, physical fitness, business and getting my life and others' together laban it will get done#
